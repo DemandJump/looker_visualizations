41b3fa8d6fa32b80c83a8f4d7f0957e8dc0287cc
Kieran <kschwegman1@gmail.com>
2019-08-31 18:00:53 -0400

    Starting on collapsible tree, and dynamically taking data in of all sizes

--- src/deployable_graphs/DJ_Collapsible_Tree.js
+++ src/deployable_graphs/DJ_Collapsible_Tree.js
@@ -133,6 +133,9 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
   console.log('Checking out query resposne dimension fields: ', queryResponse.fields.dimensions);
   console.log('Checking out query resposne measure fields: ', queryResponse.fields.measures);
 
+  console.log('Checking out query resposne dimension fields: ', queryResponse.fields.dimensions.length);
+  console.log('Checking out query resposne measure fields: ', queryResponse.fields.measures.length);
+
 
 
 
