7b92ca706a6f3912c5b6fbe3e818a06540d60795
Kieran <kschwegman1@gmail.com>
2019-08-31 17:55:54 -0400

    Starting on collapsible tree, and dynamically taking data in of all sizes

--- src/deployable_graphs/DJ_Collapsible_Tree.js
+++ src/deployable_graphs/DJ_Collapsible_Tree.js
@@ -121,16 +121,20 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
       html += LookerCharts.Utils.htmlForCell(cell);
       console.log(' cell d[0]: ', cell);
       console.log('of row: ', row);
-      console.log('Checking out query resposne fields: ', row[queryResponse.fields]);
     }     
     
-
 		for(let row of data) {
 			let cell = row[queryResponse.fields.measures[0].name];
       html += LookerCharts.Utils.htmlForCell(cell);
       console.log(' cell d[0]: ', cell);
       console.log('of row: ', row);
 		}
+
+  console.log('Checking out query resposne dimension fields: ', row[queryResponse.fields.dimensions]);
+  console.log('Checking out query resposne measure fields: ', row[queryResponse.fields.measures]);
+
+
+
 
 
     /**********************
