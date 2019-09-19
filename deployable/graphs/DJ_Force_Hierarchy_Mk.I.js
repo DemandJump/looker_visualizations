ea300f9a9c61cfe77c9cf4d1185023dd8b612001
Kieran Schwegman <kschwegman@demandjump.com>
2019-09-19 10:13:57 -0400

    Adding scaling to the neo4j chart

--- deployable/graphs/DJ_Force_Hierarchy_Mk.I.js
+++ deployable/graphs/DJ_Force_Hierarchy_Mk.I.js
@@ -222,7 +222,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
 
         // Predefined parameters for the forceSimulation
     let attractForce = d3.forceManyBody().strength(200).distanceMax(400).distanceMin(60) // Physics for node interaction
-    let repelForce = d3.forceManyBody().strength(-4040).distanceMax(4000).distanceMin(500) //.theta(.9) //*!Play with max depth and leaf nodes to determine the min/max maybe?!*
+    let repelForce = d3.forceManyBody().strength(-8040).distanceMax(4000).distanceMin(500) //.theta(.9) //*!Play with max depth and leaf nodes to determine the min/max maybe?!*
         // Initialize the simulation // 
     let simulation = d3.forceSimulation()
         .force('center', d3.forceCenter(width / 2, height / 2))
