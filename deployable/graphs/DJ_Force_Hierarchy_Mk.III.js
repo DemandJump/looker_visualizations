637c6424dc46543b5aad4f60b7d45057003fd2fb
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-02 09:56:07 -0400

    Out of nowhere my physics stopped functioning correctly.. This is really weird because I reverted the versioning back multiple days and still found no version where this was working like it was early yesterday. Searching for a solution, this actually doesn't make any sense! Even checked the version I shared on freaky friday and the visualization's physics are still doing the same thing

--- deployable/graphs/DJ_Force_Hierarchy_Mk.III.js
+++ deployable/graphs/DJ_Force_Hierarchy_Mk.III.js
@@ -545,7 +545,7 @@ function update() { /* Initialize some parameters that we will need for */
         let clickDate = new Date(); // This is to see if you double click for dragended
         let difference_ms;
         function dragstarted(d) { // On click to start the physics
-            if (!d3.event.active) { simulation.alphaTarget(0.020).alpha(.4).restart(); }
+            if (!d3.event.active) { simulation.alphaTarget(0.020).restart(); }
             d.fx = d.x;
             d.fy = d.y;
         }
