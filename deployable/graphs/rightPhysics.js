146e1b33e2dac4b0716a0cad38700e627ad1c879
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-02 08:47:00 -0400

    Fixed the clicking functionality, it should do what it's built to do. Hopefully the simulate click event doesn't trigger double clicking to focus the root node every time

--- deployable/graphs/rightPhysics.js
+++ deployable/graphs/rightPhysics.js
@@ -131,7 +131,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
     // This function is called any time the chart is supposed to visualize changes, or when any other event happens that might affect how your chart is rendered.
     
                             /* CURRENT VERSION */ 
-    console.log('Trying to get it to not break upon startup with a runtime hell loop, and instantiate the nodes in their proper positions ');
+    console.log('Checking now');
         // Just comment what your doing becuase looker takes forever to update server js file
 
         // Try implementing d3
