0b3365cc2e0ccd414dc6a9044bcbb03bbe5aa596
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-04 09:10:31 -0400

    Setting up the functionality to pull in the unique dimensions to construct the ads based on what the users put in each dimension

--- deployable/graphs/google_Ads.js
+++ deployable/graphs/google_Ads.js
@@ -1,81 +1,98 @@
-    looker.plugins.visualizations.add({
-        options: {
-            font_size: {
-                type: "string",
-                label: "Font Size",
-                values: [
-                    {"Large": "large"},
-                    {"Small": "small"}
-                ],
-                display: "radio",
-                default: "large"
-            }
-        },
-        // Onto the create section 
-    create: function(element, config) {
-        // Element is the Dom element that looker would like us to put our visualization into
-            // Looker formats it to the proper size, you just need to put the stuff here
-    // We're essentially using vanilla javascript to create a visualization for looker to append!
-
-        // Insert a <style> tag with some styles we'll use later.
-        var css = element.innerHTML = `
-            <style>
-                .hello-world-vis { 
-                    // Vertical centering
-                    height: 100%;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center; 
-                    text-align: center;
-                }
-                .hello-world-text-small {
-                    font-size: 18px;
-                }
-                .hello-world-text-large { 
-                    font-size: 72px;
-                }
-            </style>
-            `;
-
-        // Create a container element to let us center the text.
-        var container = element.appendChild(document.createElement('div'));
-        container.className = "hello-world-vis";
-
-        // Create an element to contain the text 
-            // -> This is a selector reference variable for updateASync
-        this._textElement = container.appendChild(document.createElement('div'));
-
-
-
-        /* 
-            So create is where you setup the visualization, then we render it in updateAsync
-                Note: Create is a convenient place t o do setup that only needs to happen once 
-        */
-
-    },
-        // Onto the update async section
-    updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
-        // This helps us visualize the interactive data!
-        // This function is called any time the chart is supposed to visualize changes, or when any other event happens that might affect how your chart is rendered.
-
-        /**********************
-         * Error Clauses 
-        **********************/
-            // You can clean up the error console as you iterate or even create custom errors
-        // Clear any errors from previous updates.
-        this.clearErrors();
-
-            // Create different cases for potential errors that could occur
-        // Throw some errors and exit if the shape of the data isn't what this chart needs.
-        if (queryResponse.fields.dimensions.length == 0) {
-            this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
-            return;
+looker.plugins.visualizations.add({
+    options: {
+        font_size: {
+            type: "string",
+            label: "Font Size",
+            values: [
+                {"Large": "large"},
+                {"Small": "small"}
+            ],
+            display: "radio",
+            default: "large"
         }
+    },
+    // Onto the create section 
+create: function(element, config) {
+    // Element is the Dom element that looker would like us to put our visualization into
+        // Looker formats it to the proper size, you just need to put the stuff here
+// We're essentially using vanilla javascript to create a visualization for looker to append!
+
+    // Insert a <style> tag with some styles we'll use later.
+    var css = element.innerHTML = `
+        <style>
+            .hello-world-vis { 
+                // Vertical centering
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center; 
+                text-align: center;
+            }
+            .hello-world-text-small {
+                font-size: 18px;
+            }
+            .hello-world-text-large { 
+                font-size: 72px;
+            }
+        </style>
+        `;
+
+    // Create a container element to let us center the text.
+    var container = element.appendChild(document.createElement('div'));
+    container.className = "hello-world-vis";
+
+    // Create an element to contain the text 
+        // -> This is a selector reference variable for updateASync
+    this._textElement = container.appendChild(document.createElement('div'));
 
 
-        /***********************************
-         * Update the Visualization *
-        ***********************************/
+
+    /* 
+        So create is where you setup the visualization, then we render it in updateAsync
+            Note: Create is a convenient place t o do setup that only needs to happen once 
+    */
+
+},
+    // Onto the update async section
+updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+    /*************************************
+     * Setting up the Dimension Options
+    *************************************/
+    let options = {};
+        // Create an option for each measure in your query
+    queryResponse.fields.dimensions.foreach(field =>  {
+        id = 'Id_' + field.name;
+        options[id] = 
+        {
+            type: 'string',
+            label: field.label_short,
+            default: '#887DAB',
+            display: 'color'
+        }
+    })
+    this.trigger('registerOptions', options) // register options with parent page to update visConfig
+
+
+
+
+    /**********************
+     * Error Clauses 
+    **********************/
+        // You can clean up the error console as you iterate or even create custom errors
+    // Clear any errors from previous updates.
+    this.clearErrors();
+
+        // Create different cases for potential errors that could occur
+    // Throw some errors and exit if the shape of the data isn't what this chart needs.
+    if (queryResponse.fields.dimensions.length == 0) {
+        this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
+        return;
+    }
+
+
+    /***********************************
+     * Update the Visualization *
+    ***********************************/
                             /* CURRENT VERSION */ 
     console.log('\n\n\n\n\nReplicating the google ads based on a search query the same as you would see when you search for a thing online.');
     // Just comment what your doing becuase looker takes forever to update server js file
@@ -91,28 +108,29 @@ console.log('config', config);
 console.log('queryResponse', queryResponse);
 console.log('details', details);
 
-        // Playing with dimensions and measures
-        console.log('Checking out query resposne dimension fields: ', queryResponse.fields.dimensions);
-        console.log('Checking out query resposne measure fields: ', queryResponse.fields.measures);
-            // The selector references for dimensions and length 
-        let dimensions = queryResponse.fields.dimensions;
-        let measures = queryResponse.fields.measures;
+    // Playing with dimensions and measures
+    console.log('Checking out query resposne dimension fields: ', queryResponse.fields.dimensions);
+    console.log('Checking out query resposne measure fields: ', queryResponse.fields.measures);
+        // The selector references for dimensions and length 
+    let dimensions = queryResponse.fields.dimensions;
+    let measures = queryResponse.fields.measures;
 
 
-        /**********************
-         * Update the Options
-        **********************/
-        // Here's a check we add to the end of the update function to implement the options 
-        if (config.font_size == "small") {
-            this._textElement.className = "hello-world-text-small";
-          }
-        if (config.font_size == "large") {
-            this._textElement.className = "hello-world-text-large";
-          }
-
-
-        /**************** Done! *****************/
-        // Always call done to indicate a visualization has finished rendering
-        doneRendering() 
+    /**********************
+     * Update the Options
+    **********************/
+    // Here's a check we add to the end of the update function to implement the options 
+    if (config.font_size == "small") {
+        this._textElement.className = "hello-world-text-small";
     }
+    if (config.font_size == "large") {
+        this._textElement.className = "hello-world-text-large";
+    }
+
+
+    /**************** Done! *****************/
+    // Always call done to indicate a visualization has finished rendering
+    doneRendering() 
+    
+}
 });
