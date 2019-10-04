263e9f03666c97e39ad4a3fd9aee78d60783d402
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-04 11:21:59 -0400

    Added functionality to give each of the necessary pieces to build each ad options for the user to choose their dimensions that correlate with each, then we'll build the visualization at the press of a button based on that

--- deployable/graphs/google_Ads.js
+++ deployable/graphs/google_Ads.js
@@ -166,7 +166,7 @@ console.log('details', details);
 ****************************************************************/
 let visdata = data; // Here is what we use to re render the data dynamically for our visualization based on what the user chooses
 
-console.log(`this is the config.link: ${config.link}`)
+console.log(`\nthis is the config.link: ${config.link}`)
 console.log(`this is the config.domain: ${config.domain}`)
 console.log(`this is the config.label: ${config.label}`)
 console.log(`this is the config.description: ${config.description}`)
