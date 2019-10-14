9ddc4ba405152bd9ce26e1e9c4ad98f0b833a28a
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-14 15:59:02 -0400

    Trying to use absolute positioning to fix this!

--- deployable/graphs/DJ_Single_Value_Chart.js
+++ deployable/graphs/DJ_Single_Value_Chart.js
@@ -58,17 +58,18 @@ create: function(element, config) {
         // Insert a <style> tag with some styles we'll use later.
     element.innerHTML = `
           <style>  
+            html { font-family: Roboto; font-weight: lighter; text-align: center; }
             .value:hover { text-decoration: underline; }  
             #vis { padding: 0; margin: 0; }
             iframe { padding: 0; margin: 0; }
           </style>
 
           <div class="container" style="margin: auto;  position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-              <div class="value" style="margin: auto;  text-align: center;  font-size: 4.5rem; font-style: normal; font-weight: lighter;"></div>
-              <div class="title" style="margin: auto;  text-align: center;  color: rgba(58, 66, 69, 0.5);  font-size: 1.6rem; font-weight: lighter;"></div>
+              <div class="value" style="margin: auto;  font-size: 4.5rem;  font-style: normal;"></div>
+              <div class="title" style="margin: auto;  font-size: 1.6rem;  color: rgba(58, 66, 69, 0.5);"></div>
           </div>
           
-          <div class="header" style="margin: auto;  text-align: center;  color: rgba(58, 66, 69, 0.5);  font-size: 1.4rem; font-weight: lighter;  position: absolute; left: 50%; transform: translateX(-50%); bottom: 0%;">footer.dimension thingyma</div>
+          <div class="header" style="margin: auto;  font-size: 1.4rem;  color: rgba(58, 66, 69, 0.5); position: absolute; left: 50%; transform: translateX(-50%); bottom: 0%;">footer.dimension thingydude</div>
     `;
 
     
