15d32fbcbdad58dce3a7a4db6954b9fa34d32259
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-14 15:26:48 -0400

    It centered properly, now working on teh footer title that describes teh dimension it pulled teh data from.

--- deployable/graphs/DJ_Single_Value_Chart.js
+++ deployable/graphs/DJ_Single_Value_Chart.js
@@ -54,7 +54,7 @@ create: function(element, config) {
               <div class="title" style="margin: auto;  text-align: center;  color: rgba(58, 66, 69, 0.65);  font-size: 1rem;"></div>
           </div>
           
-          <div class="header" style="margin: auto;  text-align: center;  color: rgba(58, 66, 69, 0.65);  font-size: 1rem; padding: 1rem;">footer.dimension thingyma</div>
+          <div class="header" style="margin: auto;  text-align: center;  color: rgba(58, 66, 69, 0.65);  font-size: 1rem; padding: 1rem;  position: absolute; bottom: 10%;">footer.dimension thingyma</div>
     `;
 
     d3.select(element)
