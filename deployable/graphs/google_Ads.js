7535f6721e99413cb7b1de27eb3877498f929cb4
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-16 12:42:10 -0400

    Fixing the css classing for the google ads, it was really really messy.

--- deployable/graphs/google_Ads.js
+++ deployable/graphs/google_Ads.js
@@ -20,6 +20,9 @@ create: function(element, config) {
     // Insert a <style> tag with some styles we'll use later.
     element.innerHTML = `
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+
 html, body { 
   margin: 0;
   padding: 0; 
