8dc3a3422e7c80e868c51cb1145432067a52f9db
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-21 17:35:17 -0400

    Trying to fix the translation of just the leaf nodes

--- deployable/graphs/DJ_Collapsible_Tree.js
+++ deployable/graphs/DJ_Collapsible_Tree.js
@@ -565,20 +565,16 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
   // Update the node attributes and style
   nodeUpdate.select('circle.node')
     .attr("r", d => d.children || d._children ? '25px' : '12.5px' )
-    .style('fill', d => {
-        return d._children ? "#008CCD" 
-        : !d._children && !d.children ? "#FEBF43" 
-        : "#999999"
-    })
-    .style('fill', d => {
-      if (!d._children && !d.children) { return config.djdh_measures }
-      if (d.mCount) { return config.djdh_measures }
-      for(i = 0; i < maxDepth; i++) {
-        if (i == d.depth) {
-          return chosenColors[i]
-        }
-      }
-    })
+    // .style('fill', d => {
+    //     return d._children ? "#008CCD" 
+    //     : !d._children && !d.children ? "#FEBF43" 
+    //     : "#999999"
+    // })
+    .style('fill', d => chosenColors[d.depth])
+    // .style('fill', d => {
+    //   if (!d._children && !d.children) { return config.djdh_measures }
+    //   else { return chosenColors[d.depth] }
+    // })
     // .style('stroke', d => {
     //     return d.children ? "#008CCD" :
     //     "#999999"
