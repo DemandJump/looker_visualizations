1d06d2522fb0fef18ea45c0e34081012ade4b8b3
Kieran Schwegman <kschwegman@demandjump.com>
2019-10-21 17:57:27 -0400

    Fixed the issue, it had something to do with the execution scope and the weirdness of the leaf nodes I instantiated

--- deployable/graphs/DJ_Collapsible_Tree.js
+++ deployable/graphs/DJ_Collapsible_Tree.js
@@ -565,12 +565,12 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
   // Update the node attributes and style
   nodeUpdate.select('circle.node')
     .attr("r", d => d.children || d._children ? '25px' : '12.5px' )
-    // .style('fill', d => {
-    //     return d._children ? "#008CCD" 
-    //     : !d._children && !d.children ? "#FEBF43" 
-    //     : "#999999"
-    // })
-    .style('fill', d => chosenColors[d.depth])
+    .style('fill', d => {
+        return d._children ? chosenColors[d.depth] // "#008CCD" 
+        : !d._children && !d.children ? chosenColors[d.depth] // "#FEBF43" 
+        : "#999999"
+    })
+    // .style('fill', d => chosenColors[d.depth])
     // .style('stroke', d => {
     //     return d.children ? "#008CCD" :
     //     "#999999"
