b69ac925540b1ae1ada54a21d55ffffe3bf53962
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-04 15:37:44 -0500

    Ensuring that it refactors after the zoom function to pull in the new node radius data to space the text accordingly

--- deployable/graphs/DJ_Circle_Packing.js
+++ deployable/graphs/DJ_Circle_Packing.js
@@ -449,7 +449,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
         diameter = d.nr * 2, // This is the width of the circle that's encapsulating the text
         tedit = d.data.name; // Holder for text we're gonna splice and dice
 
-        console.log(`This node's charlen: ${charlen}, and diameter: ${diameter}`);
+        // console.log(`This node's charlen: ${charlen}, and diameter: ${diameter}`);
         // console.log(`This node's charname ${tedit}, and charcount: ${charcount}`);
 
         if (charlen <= diameter || d.depth == 1) { // No editing needed, return just the data1
