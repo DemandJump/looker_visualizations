2946bdda29f88950d7b44a08af2baddda8a3ac50
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-05 12:51:08 -0500

    Fixing the zoom function with refactor

--- deployable/graphs/DJ_Circle_Packing.js
+++ deployable/graphs/DJ_Circle_Packing.js
@@ -277,7 +277,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
                 .attr('stroke-width', d => `${d.height}px`); 
             }) // Highight the border based hover
             .on("mouseout", function() { d3.select(this).attr("stroke", null); }) // Remove the highlight as you pass over
-            .on("click", d => focus !== d && (zoom(d), d3.event.stopPropagation())) // Stop other events and run the zoom function
+            .on("click", d => focus !== d && (zoom(d))) // Stop other events and run the zoom function
             .on("click", d => focus !== d && (refactor(d)));  
 
     const label = svg.append("g")
