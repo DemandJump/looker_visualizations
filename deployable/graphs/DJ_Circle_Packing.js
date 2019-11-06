4cf5669dd272c1cdac96c6266ac2bbaf708f3e25
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-06 16:12:33 -0500

    Pulls data dynamically based on whether there are pivots to the data or not

--- deployable/graphs/DJ_Circle_Packing.js
+++ deployable/graphs/DJ_Circle_Packing.js
@@ -659,7 +659,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
             if (textBox == 4) console.log('Error: word left out of textboxes: ', word);
         } // End of for loop 
 
-
+        console.log('this is d', d);
         if (d.depth == 0) { 
             d.data.text1 = d.data.name;
             d.data.textuse = 1; 
