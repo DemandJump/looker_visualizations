0438351162d2043767482c410d5362eb80b4656d
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-06 15:04:59 -0500

    Found the element, adding in auto height attr to see it in the vis

--- deployable/graphs/DJ_Circle_Packing.js
+++ deployable/graphs/DJ_Circle_Packing.js
@@ -321,7 +321,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
                 .style("fill-opacity", d => d.parent === root ? 1 : 0)
                 .style("display", d => d.parent === root ? "inline" : "none")
                 .style("font-size", d => sizeText(d)) // This also calculates the number of text spaces each nodes uses
-                .attr('height', 'auto')
+                .style('height', 'auto')
                 .attr('dy', spaceOne)
                 .text(d => d.data.text1);
               
@@ -335,7 +335,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
                 .style('fill-opacity', d => d.parent === root ? 1 : 0)
                 .style('display', d => d.parent === root ? 'inline' : 'none')
                 .style("font-size", d => sizeText(d)) // This also calculates the number of text spaces each nodes uses
-                .attr('height', 'auto')
+                .style('height', 'auto')
                 .attr('dy', spaceTwo)
                 .text(d => d.data.text2);
 
@@ -349,7 +349,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
                 .style('fill-opacity', d => d.parent === root ? 1 : 0)
                 .style('display', d => d.parent === root ? 'inline' : 'none')
                 .style("font-size", d => sizeText(d)) // This also calculates the number of text spaces each nodes uses
-                .attr('height', 'auto')
+                .style('height', 'auto')
                 .attr('dy', spaceThree)
                 .text(d => d.data.text3);
 
@@ -363,7 +363,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
                 .style('fill-opacity', d => d.parent === root ? 1 : 0)
                 .style('display', d => d.parent === root ? 'inline' : 'none')
                 .style("font-size", d => sizeText(d)) // This also calculates the number of text spaces each nodes uses
-                .attr('height', 'auto')
+                .style('height', 'auto')
                 .attr('dy', spaceFour)
                 .text(d => d.data.text4);
 
