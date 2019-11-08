3577444d05561c49b3a0f263303432427f9ae30f
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-08 12:00:19 -0500

    Trying a bunch of different ways to pull all the null values out of the visual

--- deployable/graphs/DJ_Circle_Packing.js
+++ deployable/graphs/DJ_Circle_Packing.js
@@ -254,20 +254,6 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
     });
     console.log(`The finished min ${min}, and max ${max}`);
 
-    
-    // dimensionNames = []; // Go through every data piece with dimension names
-    // dimensions.forEach(dimension => dimensionNames.push(dimension.name));
-    // // console.log('This is the dimensionNames: ', dimensionNames)
-    // data.forEach(node => { // Now for all of the 'hierarchical' dimensions that are null, pull them out of the taxonomy, structure won't build the null values out but keep the rest of the structure together and sound
-    //     for(let i = 0; i < dimensionNames.length; i++) {
-    //         console.log(`Node dimension ${dimensionNames[i]}, with a value of: ${node[dimensionNames[i]]['value']}`);
-    //         if (node[dimensionNames[i]]['value'] == 'null' || node[dimensionNames[i]['value']] == null) {
-    //             console.log('Found null value in', node[dimensionNames[i]]);
-    //             delete node[dimensionNames[i]];
-    //         }
-    //     }
-    // });
-
 
         // Main variables for building the svg
     const burrow = this.burrow(data, taxonomyPass);
