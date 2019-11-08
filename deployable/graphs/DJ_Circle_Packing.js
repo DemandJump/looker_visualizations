c6bde82809e0030c1ef29464553a0f245e030424
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-08 09:16:08 -0500

    Changed coloring, added somefunctionality to clear null nodes out of the hierarchy

--- deployable/graphs/DJ_Circle_Packing.js
+++ deployable/graphs/DJ_Circle_Packing.js
@@ -258,8 +258,11 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
     // console.log('This is the dimensionNames: ', dimensionNames)
     data.forEach(node => { // Now for all of the 'hierarchical' dimensions that are null, pull them out of the taxonomy, structure won't build the null values out but keep the rest of the structure together and sound
         for(let i = 0; i < dimensionNames.length; i++) {
-            console.log(`Node dimension ${node[dimensionNames[i]]}, with a value of: ${node[dimensionNames[i]]['value']}`);
-            if (node[dimensionNames[i]]['value'] == 'null') delete node[dimensionNames[i]];
+            console.log(`Node dimension ${dimensionNames[i]}, with a value of: ${node[dimensionNames[i]]['value']}`);
+            if (node[dimensionNames[i]]['value'] == 'null') {
+                console.log('Found null value in', node[dimensionNames[i]]);
+                delete node[dimensionNames[i]];
+            }
         }
     });
     // console.log(`The finished min ${min}, and max ${max}`);
