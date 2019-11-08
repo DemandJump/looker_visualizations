e124c474d404ba4b436d48a7415fe609d0a87597
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-08 10:19:27 -0500

    Changed coloring, added somefunctionality to clear null nodes out of the hierarchy

--- deployable/graphs/DJ_Circle_Packing.js
+++ deployable/graphs/DJ_Circle_Packing.js
@@ -297,6 +297,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
     function reverseDepthCollapse(datum, dep) {
         if (datum.children && datum.depth != dep) datum.children.forEach(reverseDepthCollapse); // Parse down into all the nodes of specified deoth given
         if (datum.depth == dep) { // When you reach desired depth, delete all nodes that are null from the visual before we instantiate it
+            console.log(`Parsed into desired depth ${dep}`, datum);
             if (datum.data.name == 'null') delete datum;
             if (datum.data.name == null) delete datum;
         }
