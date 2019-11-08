4507e61f7eef37caa613ce828d237fc312ef9ad2
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-08 09:39:38 -0500

    Changed coloring, added somefunctionality to clear null nodes out of the hierarchy

--- deployable/graphs/DJ_Circle_Packing.js
+++ deployable/graphs/DJ_Circle_Packing.js
@@ -255,18 +255,18 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
     console.log(`The finished min ${min}, and max ${max}`);
 
     
-    dimensionNames = []; // Go through every data piece with dimension names
-    dimensions.forEach(dimension => dimensionNames.push(dimension.name));
-    // console.log('This is the dimensionNames: ', dimensionNames)
-    data.forEach(node => { // Now for all of the 'hierarchical' dimensions that are null, pull them out of the taxonomy, structure won't build the null values out but keep the rest of the structure together and sound
-        for(let i = 0; i < dimensionNames.length; i++) {
-            console.log(`Node dimension ${dimensionNames[i]}, with a value of: ${node[dimensionNames[i]]['value']}`);
-            if (node[dimensionNames[i]]['value'] == 'null' || node[dimensionNames[i]['value']] == null) {
-                console.log('Found null value in', node[dimensionNames[i]]);
-                delete node[dimensionNames[i]];
-            }
-        }
-    });
+    // dimensionNames = []; // Go through every data piece with dimension names
+    // dimensions.forEach(dimension => dimensionNames.push(dimension.name));
+    // // console.log('This is the dimensionNames: ', dimensionNames)
+    // data.forEach(node => { // Now for all of the 'hierarchical' dimensions that are null, pull them out of the taxonomy, structure won't build the null values out but keep the rest of the structure together and sound
+    //     for(let i = 0; i < dimensionNames.length; i++) {
+    //         console.log(`Node dimension ${dimensionNames[i]}, with a value of: ${node[dimensionNames[i]]['value']}`);
+    //         if (node[dimensionNames[i]]['value'] == 'null' || node[dimensionNames[i]['value']] == null) {
+    //             console.log('Found null value in', node[dimensionNames[i]]);
+    //             delete node[dimensionNames[i]];
+    //         }
+    //     }
+    // });
 
 
         // Main variables for building the svg
