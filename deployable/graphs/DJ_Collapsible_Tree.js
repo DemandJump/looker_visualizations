d1ebcced098145f3ac0e9f51193f3962afa1b78f
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-13 16:04:02 -0500

    Fixed the grouping function. Working on the initial panning to the center of the tidy tree.

--- deployable/graphs/DJ_Collapsible_Tree.js
+++ deployable/graphs/DJ_Collapsible_Tree.js
@@ -500,11 +500,12 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
       // zoom_handler.translateBy(container, root.x, root.y);
       // d3.select('.everything').transition().duration(1000).call(zoom_handler.translateBy, root.x, root.y);
       let x = -1 * root.x;
-      let y = root.y + 54;
+      let y = root.y;
+      console.log(`New x: ${x}, and y: ${y}`);
 
       container.transition().duration(1200).call(
         zoom_handler.transform,
-        d3.zoomIdentity.translate(width / 2, height / 2).scale(1).translate(y, x)
+        d3.zoomIdentity.translate(width, height).scale(1).translate(y, x)
       );
   }
   
