210988774d18c0e9ffc6c3f332d88a876b2309b8
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-13 14:23:27 -0500

    Fixing the grouping function

--- deployable/bundled/dj_cp_vis.js
+++ deployable/bundled/dj_cp_vis.js
@@ -866,14 +866,11 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
     } // End of color by group function
     
     function parseDown(d) { // Find the phrase type or group value by parsing down the tree
-        if(d.children) {
-            if(d.children != []) { 
-                console.log('This is d currently', d);
-                parseDown(d.children[0]); 
-            }
-        } else { 
-            node.group = d.data.groupColor; 
+        if(d.children != []) { 
+            console.log('This is d currently', d);
+            parseDown(d.children[0]); 
         }
+        else { node.group = d.data.groupColor.value; }
     }
     
     function findUniqueValue(d) {
