8b23280724b2597f5066ea3d5f94b38d6f85e8d1
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-14 10:29:24 -0500

    Fixed the collapsible tree's camera to start at the center honed in on the root

--- deployable/bundled/dj_cp_vis.js
+++ deployable/bundled/dj_cp_vis.js
@@ -422,7 +422,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
 
 
     nodes.forEach(d => {
-        if(d.data.name == 'null') d.data.leaf = false;
+        if(d.data.name == 'null') { d.data.leaf = false; }
         if(d.children.length == 1 && d.children[0].data.name == 'null') { d..data.leaf = true; }
     });
 
