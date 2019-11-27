9957764e6d9b42cd81208c34a87a25977cd071bf
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-27 09:42:56 -0500

    Recreating the configuration, you get to build it brick by brick now

--- deployable/bundled/dj_cp_vis.js
+++ deployable/bundled/dj_cp_vis.js
@@ -438,8 +438,8 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
     root.children[0].data.id = 'tether';
     root.children.forEach(collapseNulls);
     let nodes = root.descendants().slice(1);
-    unpackageData(); // This edits the nodes and unpackages the concatenated data
     root.data.name = root.children[0].data.name; // Grab the unpackaged data's name
+    unpackageData(); // This edits the nodes and unpackages the concatenated data
 
 
     findActualLeafNodes(); // Find all new leaf nodes and use a variable to denote them for the d3 hierarchy
