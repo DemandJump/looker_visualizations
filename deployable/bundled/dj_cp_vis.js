504d1a4dab4511effe20e71556165f4989770e10
Kieran Schwegman <kschwegman@demandjump.com>
2019-11-27 13:24:28 -0500

    Implementing breadcrumb function

--- deployable/bundled/dj_cp_vis.js
+++ deployable/bundled/dj_cp_vis.js
@@ -226,16 +226,20 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
 
             // Init the breadcrumbs
         for(let i = 0; i < breadCrumbIds.length; i++) {
-            let h = i * 40;
             d3.select('.breadcrumbContainer').append('div')
                 .attr('class', 'breadcrumbs')
                 .attr('id', breadCrumbIds[i])
                 .style('position', 'absolute')
-                .style('top', h)
+                .style('top', i * 40)
                 .style('left', 0)
                 .style('height', '40px')
-                .html(`<style>top: ${h};</style>Breadcrumb number ${i}`);
+                .html(`Breadcrumb number ${i}`);
         } // end of for loop
+        for(let i = 0; i < breadCrumbIds.length; i++) {
+            let h = i * 40;
+            let id = `#${breadCrumbIds[i]}`;
+            d3.select(id).style('top', h);
+        }
 
         let id = `#bc${d.depth}`;
         d3.select(id).html(d.data.name);
