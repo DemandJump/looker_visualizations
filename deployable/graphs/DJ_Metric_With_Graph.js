bed155af6a6f786a5ad7d0b1da4731d7086084b1
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-03 18:43:34 -0500

    Appending data to graph

--- deployable/graphs/DJ_Metric_With_Graph.js
+++ deployable/graphs/DJ_Metric_With_Graph.js
@@ -48,6 +48,34 @@ looker.plugins.visualizations.add({
             .attr('class', 'svg')
             .attr('position', 'relative');
 
+
+        this._label = d3.select('.container').append('div')
+            .attr('class', 'label')
+            .attr('text-align', 'left')
+            .style('position', 'absolute')
+            .style('top', '10%')
+            .style('left', '50%')
+            .style('transform', 'translate(-50%, -50%)')
+            .html('The Label!');
+
+        this._metric = d3.select('.container').append('div')
+            .attr('class', 'metric')
+            .attr('text-align', 'center')
+            .style('position', 'absolute')
+            .style('top', '50%')
+            .style('left', '50%')
+            .style('transform', 'translate(-50%, -50%)')
+            .html('This is the metric');
+
+        this._labelm = d3.select('.container').append('div')
+            .attr('class', 'labelm')
+            .attr('text-align', 'center')
+            .style('position', 'absolute')
+            .style('top', '60%')
+            .style('left', '50%')
+            .style('transform', 'translate(-50%, -50%)')
+            .html('This is the metric label');
+
     },
    
     updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
@@ -86,40 +114,6 @@ looker.plugins.visualizations.add({
 
         let containerWidth = window.innerWidth;
         let containerHeight = window.innerHeight;
-
-
-        let label = d3.select('.container').append('div')
-            .attr('class', 'label')
-            .attr('text-align', 'left')
-            .style('position', 'absolute')
-            .style('top', '10%')
-            .style('left', '50%')
-            .style('transform', 'translate(-50%, -50%)')
-            // .style('width', width)
-            // .style('height', '40px')
-            .html('The Label!');
-
-        let metric = d3.select('.contianer').append('div')
-            .attr('class', 'metric')
-            .attr('text-align', 'center')
-            .style('position', 'absolute')
-            .style('top', '50%')
-            .style('left', '50%')
-            .style('transform', 'translate(-50%, -50%)')
-            // .style('width', width)
-            // .style('height', '40px')
-            .html('This is the metric');
-
-        let labelm = d3.select('.contianer').append('div')
-            .attr('class', 'labelm')
-            .attr('text-align', 'center')
-            .style('position', 'absolute')
-            .style('top', '60%')
-            .style('left', '50%')
-            .style('transform', 'translate(-50%, -50%)')
-            // .style('width', width)
-            // .style('height', '20px')
-            .html('This is the metric label');
 
 
             // Create the min and max of each of the axes 
@@ -170,7 +164,7 @@ looker.plugins.visualizations.add({
             .attr("class", "y axis")
             .call(yAxis);
 
-            
+
         label.html(dimensions[0].label_short);
         metric.html(data[0][dimensions[0].name].value);
         labelm.html(measures[0].label_short);
