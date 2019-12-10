46b39a27ae9abcbc55850779224a1fe1695c9f5d
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-10 17:53:31 -0500

    Implementing pivot use case for this graph

--- deployable/graphs/DJ_Collapsible_Tree.js
+++ deployable/graphs/DJ_Collapsible_Tree.js
@@ -258,14 +258,14 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
 
   // console.log('root', root);
     // Collapse the nodes, or comment this out to see the whole layout
-  // root.children.forEach(collapse);
-  // function collapse(d) {
-  //   if(d.children) {
-  //     d._children = d.children
-  //     d._children.forEach(collapse)
-  //     d.children = null
-  //   }
-  // }
+  root.children.forEach(collapse);
+  function collapse(d) {
+    if(d.children) {
+      d._children = d.children
+      d._children.forEach(collapse)
+      d.children = null
+    }
+  }
   
   container.call(zoom_handler);
   let updatInit = 0;
