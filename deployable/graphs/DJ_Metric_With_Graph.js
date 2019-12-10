448958924aef9a46b8c5c92b1299c51d66e92c44
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-10 14:11:54 -0500

    Implementing pivot use case for this graph

--- deployable/graphs/DJ_Metric_With_Graph.js
+++ deployable/graphs/DJ_Metric_With_Graph.js
@@ -127,7 +127,9 @@ looker.plugins.visualizations.add({
             for(let i = 0; i < iterations; i++) {
                 data[i]['value1'] = prevPer[i];
                 data[i]['value0'] = currPer[i];
-                data[i][measures[0].name]['Previous Periods'] = currPer[i];
+
+                console.log('parsing to prev period', data[i][measures[0].name]['Previous Period']);
+                data[i][measures[0].name]['Previous Period'] = currPer[i];
             }
             if (calculation == 'pivot') newData = data.slice(0, iterations * -1);
             console.log('This is the new data', newData);
