7d4f88a18c54ecb2a0ed4deabc8dfe0cf5982fe4
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-11 13:59:44 -0500

    Reformatting the elements of the value chart

--- deployable/graphs/Dj_Remake_Single_Value_Chart.js
+++ deployable/graphs/Dj_Remake_Single_Value_Chart.js
@@ -312,10 +312,11 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
               <div class="djvsTitle" style="margin: auto;  display: block;  position: relative;  font-size: 1.6rem;  color: #A5A9AA;"></div>
           </div>
 
-          <!--
+
           <div class="djvsHeader" style="margin: auto; font-size: 1.4rem;  color: #A5A9AA; position: absolute; left: 50%; transform: translateX(-50%); bottom: 0%;  width: 100%;  padding: 10px;">selected dimension</div>
-          -->
+          <!--
           <div class="djvsHeader" style="margin: auto;  display: block;  font-size: 1.4rem;  color: #A5A9AA;  vertical-align: bottom;  width: 100%;  padding: 10px;">selected dimension</div>
+          -->
 
       `;
   }
