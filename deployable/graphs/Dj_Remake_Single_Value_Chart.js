955ed2ed27ca6b1c9b08e4476be0846ab038a68f
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-12 15:30:41 -0500

    Fixing the styling and spacing of the elements

--- deployable/graphs/Dj_Remake_Single_Value_Chart.js
+++ deployable/graphs/Dj_Remake_Single_Value_Chart.js
@@ -289,6 +289,8 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
   if (window.innerHeight < spacing) element_spacing = 'dynamic_size';
 
 
+  let elementWidth = window.innerWidth * .9; 
+  let elementHeight = window.innerHeight * .9;
   if (element_spacing == 'word_break') {
       element.innerHTML = `
           <style>  
@@ -313,7 +315,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
               div { font-weight: 300; font-family: 'Roboto'; margin: 0; padding: 0; text-align: center; }
           </style>
 
-          <div class="djvsContainer" style="margin: auto;  display; inline-block; font-size: 9vw;  text-align: center; padding: 0; vertical-align: middle;">
+          <div class="djvsContainer" width: ${elementWidth}; height: ${elementHeight}; style="margin: auto;  display; inline-block; font-size: 9vw;  text-align: center; padding: 0; vertical-align: middle;">
               <div class="djvsValue" style="margin: auto;  display: block; font-size: 9vw;  font-style: normal;  vertical-align: middle;"></div>
               <div class="djvsTitle" style="margin: auto;  display: block; font-size: 1.6rem;  color: #A5A9AA;  vertical-align: middle;"></div>
           </div>
