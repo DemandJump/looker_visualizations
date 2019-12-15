6b80e3ef0d8becfe1eedf66baf45061070288864
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-15 13:33:22 -0500

    Cleaned up code, fixed errors on metric with graph

--- deployable/graphs/DJ_Collapsible_Tree.js
+++ deployable/graphs/DJ_Collapsible_Tree.js
@@ -435,7 +435,7 @@ updateAsync: function(data, element, config, queryResponse, details, doneRenderi
                 updatInit++;
                 container.transition().duration(1200).call(
                     zoom_handler.transform,
-                    d3.zoomIdentity.translate(window.innerWidth / 2, window.innerHeight / 2).scale(0.25).translate(-root.y, -root.x),
+                    d3.zoomIdentity.translate(window.innerWidth / 2, window.innerHeight / 2).scale(0.20).translate(-root.y, -root.x),
                 );
             }
         }
