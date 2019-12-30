5407247cf878cf76b0cd720e810e0955ee4add5b
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-30 14:43:28 -0500

    Configuring the table data from looker

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -160,7 +160,10 @@ looker.plugins.visualizations.add({
         let tablebody = table.append("tbody");
         let rows = tablebody.selectAll("tr")
             .data(rowData).enter().append("tr")
-                .attr('class', d => d.type);
+                .attr('class', d => {
+                    console.log('This is tr d', d);
+                    return d.type
+                });
 
         let cells = rows.selectAll("td")
             .data(d => d).enter().append("td")
