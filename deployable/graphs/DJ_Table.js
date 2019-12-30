91bbd28ec5ef0bee49d5032bcf2bd5234d555cae
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-30 14:10:34 -0500

    Configuring the table data from looker

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -67,7 +67,7 @@ looker.plugins.visualizations.add({
 
     updateAsync: function(data, element, config, queryResponse, details, doneRendering) { let d3 = d3v5;
         this._table.selectAll("*").remove(); // Clear out the data before we add the vis
-        console.log(`\n\n\n\n`);
+        console.log(`\n\n\n\n\n\n\n\n\n\n`);
         console.log(`direct reference to settings (this.options)`, this.options);
         console.log(`config`, config);
         console.log(`queryResponse`, queryResponse);
