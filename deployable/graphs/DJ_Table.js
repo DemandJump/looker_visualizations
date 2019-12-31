348b776220690a73b1aded960aa1ae201d4f26b2
Kieran Schwegman <kschwegman@demandjump.com>
2019-12-30 22:55:47 -0500

    Configuring the table data from looker

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -69,8 +69,8 @@ looker.plugins.visualizations.add({
                 }
 
                     /* Remove some spacing on the index column */
-                td.first-child {padding-left: 1px;}
-                th.first-child {padding-left: 1px;}
+                td:first-child {padding-left: 1px;}
+                th:first-child {padding-left: 1px;}
             
 
                     /* Highlight the hovered cell */
