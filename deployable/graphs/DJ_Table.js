ea101b70f2a2bfe2fd3395eb4902dde7db43d22f
Kieran Schwegman <kschwegman@demandjump.com>
2020-01-06 10:16:48 -0500

    Adding dropdown header function via LookerChart Utils

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -37,7 +37,7 @@ looker.plugins.visualizations.add({
                     margin: 0;
                     padding: 0; 
                     box-sizing: border-box;
-                    font-family: 'Roboto';
+                    font-family: Roboto;
                     color: #404040;
                 }
 
@@ -52,6 +52,7 @@ looker.plugins.visualizations.add({
                     position: relative;
                     font-weight: 300;
                     font-size: 11px;
+                    font-family: Roboto;
                     border-spacing: 0px;
                     width: 100%;
                     overflow-x: auto;
