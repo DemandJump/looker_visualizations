d02eef4102e0e4ae5143f46c595f0a306e4f261a
Kieran Schwegman <kschwegman@demandjump.com>
2020-01-06 12:57:17 -0500

    Adding dropdown header function via LookerChart Utils

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -177,7 +177,6 @@ looker.plugins.visualizations.add({
                 }
 
                 ul {
-                    display: table; 
                     margin: 0;
                     margin-block-start: 0;
                     margin-block-end: 0;
@@ -185,8 +184,6 @@ looker.plugins.visualizations.add({
 
                 .ulDropdown li {
                     padding: 10px 10px;
-                    display: table-cell;
-                    width: 100%;
                 }
 
 
