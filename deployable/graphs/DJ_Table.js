73d713045d8cf9a61f2f611905e7353177a4f70c
Kieran Schwegman <kschwegman@demandjump.com>
2020-01-30 13:31:54 -0500

    New Configuration for the visual

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -498,8 +498,8 @@ looker.plugins.visualizations.add({
               // Create the fields
               console.log('These are the settings', settings);
               for(let i = 0; i < selectFieldAmount; i++) {
-                  console.log(`settings[${name}]`, settings[name]);
                   let name = `formattedField${i}`;
+                  console.log(`settings[${name}]`, settings[name]);
                   settings[name] = {
                       order: 12.2 + (i/10),
                       section: 'Formatting',
