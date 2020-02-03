36c76f7fcda74a54cb8510a5f3092c6bf3366145
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-03 17:51:27 -0500

    fixing colorConfigureRows function

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -1026,9 +1026,9 @@ looker.plugins.visualizations.add({
 
                 // Convert hex to rgb
                 let colorOne = hexToRgb(rule.alongAScale.color1);
-                let colorTwo = hexToRgb(rule.alongAScale.color2);
-                console.log('color1', colorOne);
-                console.log('color2', colorTwo);
+                let colorTwo = hexToRgb(rule.alongAScale.color2);\
+                colorOne = `rgb(${colorOne[0]}, ${colorOne[1]}, ${colorOne[2]})`;
+                colorTwo = `rgb(${colorTwo[0]}, ${colorTwo[1]}, ${colorTwo[2]})`;
                 // grab the color steps
                 let newColorNumbers = interpolateColors(colorOne, colorTwo, 4);
                 console.log('newColorNumbers', newColorNumbers);
