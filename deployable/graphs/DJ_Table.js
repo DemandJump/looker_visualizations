b4cbdd768f7f096d9a6f7b73bd32cc8f51d39b1b
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-04 16:08:06 -0500

    Fixing rule instance error. When there are multiple instances, some styling doesn't cross over, not matter the second rule value. It just doesn't append the first rule's data in. even if the rules are the same

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -489,8 +489,14 @@ looker.plugins.visualizations.add({
 
             if (d.links) {
                 let dropDownHeader = d.links[0].type_label;
+                let italicVals = '';
+                for(let i = 0; i < ruleInstances; i++) {
+                    if (d[`rule_${rr}`]) {
+                        italicVals = italicVals + d[`rule_${rr}`].italic.toString();
+                    }
+                }
                 linkHeader = `
-                    <div class="dropdown-header">${dropDownHeader}</div> 
+                    <div class="dropdown-header">${italicVals}</div>  // dropDownHeader
                 `;
                 d.links.forEach(link => {
                     links = links + `
