8363c76558c394b61f9d3eb74a017f2ca79a4990
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-05 18:27:50 -0500

    Building area chart from architecture

--- deployable/graphs/dj_arc_area_chart.js
+++ deployable/graphs/dj_arc_area_chart.js
@@ -2,6 +2,10 @@ looker.plugins.visualizations.add({
     options: {},
     create: function(element, config) {
         element.innerHTML = `
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+
             <div class="row">
                 <div class="col-md-6">
                     <div class="main-card mb-3 card">
