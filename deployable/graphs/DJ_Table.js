c74655d1d3002a6cc7358bc4823da9506d022649
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-05 09:30:20 -0500

    And here's a setting to turn it off

--- deployable/graphs/DJ_Table.js
+++ deployable/graphs/DJ_Table.js
@@ -490,6 +490,7 @@ looker.plugins.visualizations.add({
 
             let text = `<span>${value}</span>`;
             if (config.convertImages) {
+                value = value.toString();
                 if (value.includes('data:image/')) text = `<img src="${value}">`; // Embedded check
                 if (value.substr(0,8) == 'https://') {
                     if (value.includes('.png') || value.includes('.jpg') || value.includes('images?') || value.includes('images') || value.includes('img') || value.includes('Image') || value.includes('Img')) text = `<img src="${value}"`;
