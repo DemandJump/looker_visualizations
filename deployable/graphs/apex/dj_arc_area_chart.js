c4d6142cd87eed2fe1da5c981e98c620105f09b4
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-06 15:43:14 -0500

    Adding looker data to the area chart

--- deployable/graphs/apex/dj_arc_area_chart.js
+++ deployable/graphs/apex/dj_arc_area_chart.js
@@ -27,14 +27,15 @@ looker.plugins.visualizations.add({
         let title = '';
         let subTitle = '';
         let dataSeries = {}
-        let dsd = [];
-        let dsm = [];
+        dataSeries.dsd = [];
+        dataSeries.dsm = [];
         
 
         data.forEach(row => {
             dsd.push(row[queryResponse.fields.dimensions[0].name].value);
             dsm.push(row[queryResponse.fields.measures[0].name].value);
         });
+
 
         if (this.clearElements >= 1) {
 
@@ -50,7 +51,7 @@ looker.plugins.visualizations.add({
                     "monthDataSeries1": {
                         "prices": [8107.85,8128.0,8122.9,8165.5,8340.7,8423.7,8423.5,8514.3,8481.85,8487.7,8506.9,8626.2,8668.95,8602.3,8607.55,8512.9,8496.25,8600.65,8881.1,9340.85
                         ],
-                        "dates": ["13 Nov slkdjfs2017","14 Nov 2017","15 Nov 2lk017","16 Nov 2017","17 Nov 2017","20 Nov 2017","21 Nov 2017","22 Nov 2017","23 Nov 2017","24 Nov 2017","27 Nov 2lksdf017","28 Nov 2017","29 Nov 2017","30 Nov 2017","01 Dec 2017","04 Dec 2017","05 Dec 2017","06 Dec 2017","07 Dec 2017","08 Dec 2017"
+                        "dates": ["13 Nov 2017","14 Nov 2017","15 Nov 2017","16 Nov 2017","17 Nov 2017","20 Nov 2017","21 Nov 2017","22 Nov 2017","23 Nov 2017","24 Nov 2017","27 Nov 2017","28 Nov 2017","29 Nov 2017","30 Nov 2017","01 Dec 2017","04 Dec 2017","05 Dec 2017","06 Dec 2017","07 Dec 2017","08 Dec 2017"
                         ]
                     }
                 };
