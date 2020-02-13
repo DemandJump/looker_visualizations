fbb14f519ee7bdc300423dd1cfb1a0c07dab0fb7
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-13 10:51:35 -0500

    Fixing area chart data pull error

--- deployable/graphs/apex/dj_arc_area_chart.js
+++ deployable/graphs/apex/dj_arc_area_chart.js
@@ -245,7 +245,7 @@ looker.plugins.visualizations.add({
         datum.forEach(row => {
             xaxis.push(row[queryResponse.fields.dimension_like[0].name].value);
             for(let i = 0; i < queryResponse.fields.measure_like.length; i++) {
-                seriesData.data.push(row[queryResponse.fields.measure_like[i].name].value));
+                seriesData.data.push(row[queryResponse.fields.measure_like[i].name].value);
                 let ob = {};
                 let xVal;
                 let yVal;
