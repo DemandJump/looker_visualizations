6bbfe77e2528d76aa90d3e6d8847860c2d8777e7
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-19 15:24:12 -0500

    Adding new drilldown menu links

--- deployable/graphs/apex/dj_arc_area_chart.js
+++ deployable/graphs/apex/dj_arc_area_chart.js
@@ -584,28 +584,21 @@ looker.plugins.visualizations.add({
                 let cData = document.getElementById(cid);
                 let ps = cData.getBoundingClientRect();
 
-                function getOffset(el){
-                    var el2 = el;
-                    var curtop = 0;
-                    var curleft = 0;
-                    if (document.getElementById || document.all) {
-                        do  {
-                            curleft += el.offsetLeft-el.scrollLeft;
-                            curtop += el.offsetTop-el.scrollTop;
+                function getOffset( el ) {
+                    var _x = 0;
+                    var _y = 0;
+                    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+                        _x += el.offsetLeft - el.scrollLeft;
+                        _y += el.offsetTop - el.scrollTop;
+                        // chrome/safari
+                        if ($.browser.webkit) {
+                            el = el.parentNode;
+                        } else {
+                            // firefox/IE
                             el = el.offsetParent;
-                            el2 = el2.parentNode;
-                            while (el2 != el) {
-                                curleft -= el2.scrollLeft;
-                                curtop -= el2.scrollTop;
-                                el2 = el2.parentNode;
-                            }
-                        } while (el.offsetParent);
-                
-                    } else if (document.layers) {
-                        curtop += el.y;
-                        curleft += el.x;
+                        }
                     }
-                    return [curtop, curleft];
+                    return { top: _y, left: _x };
                 }
                 console.log(`This is the holder of holder`, circleHolder);
                 console.log(`This is the holder of the circle`, circle);
