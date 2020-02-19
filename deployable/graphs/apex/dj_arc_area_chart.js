f166385a7e3cb055517e4938c752ffc68b001321
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-19 12:07:53 -0500

    Adding new drilldown menu links

--- deployable/graphs/apex/dj_arc_area_chart.js
+++ deployable/graphs/apex/dj_arc_area_chart.js
@@ -583,7 +583,7 @@ looker.plugins.visualizations.add({
 
             let valueData = {name: xaxis[i], yValues: []};
             queryResponse.pivots.forEach((row, index) => {
-                let name = row.data[queryResponse.fields.pivots[0].name].replace(` `, `-`);
+                let name = row.data[queryResponse.fields.pivots[0].name].replace(/ /g, `-`);
                 console.log(`current name:`, name);
                 let holder = document.getElementsByClassName(`apexcharts-series ${name}`);
                 console.log(`This is the current holder data`, holder);
