45c12417365bb284012f923491fb1364fa23825a
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-19 15:11:21 -0500

    Adding new drilldown menu links

--- deployable/graphs/apex/dj_arc_area_chart.js
+++ deployable/graphs/apex/dj_arc_area_chart.js
@@ -584,10 +584,18 @@ looker.plugins.visualizations.add({
                 let cData = document.getElementById(cid);
                 let ps = cData.getBoundingClientRect();
 
-                let jcir = $(`#${cid}`);
-                console.log(`This is jcir`, jcir[0]);
-                console.log(`Position of element`, jcir[0].position);
-                console.log(`offset of element`, jcir[0].offset);
+                function getOffset( el ) {
+                    var _x = 0;
+                    var _y = 0;
+                    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+                        _x += el.offsetLeft - el.scrollLeft;
+                        _y += el.offsetTop - el.scrollTop;
+                        el = el.offsetParent;
+                    }
+                    return { top: _y, left: _x };
+                }
+                var x = getOffset( document.getElementById(cid) ); 
+                console.log(`This is get offset`, x);
 
                 let data = {
                     pivot: name,
