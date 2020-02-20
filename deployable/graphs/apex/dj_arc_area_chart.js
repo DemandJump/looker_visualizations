62a17ecdc39a5a4f285744a5402d5a8b57da8498
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-20 14:54:34 -0500

    Cleaning up a few bugs, improving performance, appending series event listeners for drilldown

--- deployable/graphs/apex/dj_arc_area_chart.js
+++ deployable/graphs/apex/dj_arc_area_chart.js
@@ -705,8 +705,8 @@ looker.plugins.visualizations.add({
             let singleSeries = seriesSection.enter().append(`div`);  
             seriesSection.merge(singleSeries)
                 .attr(`class`, `measures`)
-                .style(`width`, data => `${data.coordinates.width * .75}px`)
-                .style(`height`, data => `${data.coordinates.height * .75}px`)
+                .style(`width`, data => `${data.coordinates.width}px`)
+                .style(`height`, data => `${data.coordinates.height}px`)
                 .style(`z-index`, `22`)
                 .style(`position`, `absolute`)
                 .style(`left`, data => `${data.coordinates.left - 3}px`)
