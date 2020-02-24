880871296ed0c3d6dc04fb00af252430f7e18996
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-24 11:08:14 -0500

    Fixing a few errors

--- deployable/graphs/apex/dj_arc_area_chart.js
+++ deployable/graphs/apex/dj_arc_area_chart.js
@@ -564,8 +564,8 @@ looker.plugins.visualizations.add({
             console.log(`xaxis data`, xaxis);
             configuration[`series`] = seriesData;
             configuration[`labels`] = axisData;
-            configuration[`xaxis`] = {type: `datetime`}; // category, numeric, datetime
-            // configuration[`xaxis`] = {type: `category`}; // Look automatically formats dates, and we pull in a random amount of dynamically mades dates that are already formatted. Category pulls in those formats and it doesn't break trying to reformat the data
+            // configuration[`xaxis`] = {type: `datetime`}; // category, numeric, datetime
+            configuration[`xaxis`] = {type: `category`}; // Look automatically formats dates, and we pull in a random amount of dynamically mades dates that are already formatted. Category pulls in those formats and it doesn't break trying to reformat the data
             configuration[`yaxis`] = {opposite: true};
             configuration[`legend`] = {horizontalAlign: alignLegend};
         }
