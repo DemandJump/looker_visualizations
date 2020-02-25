eb312c726db27892116e81880685ad3a1792da63
Kieran Schwegman <kschwegman@demandjump.com>
2020-02-25 12:31:56 -0500

    Changing the drilldown naming convention to match the series

--- deployable/graphs/apex/dj_arc_vertical_columns.js
+++ deployable/graphs/apex/dj_arc_vertical_columns.js
@@ -451,7 +451,7 @@ looker.plugins.visualizations.add({
             for(let i = 0; i < seriesData.length; i++) {
                 if (seriesData[i].links[index] !== undefined) {
                     for(let j = 0; j < seriesData[i].links[index].length; j++) {
-                        if (!seriesData[i].links[index][j][`type_label`]) seriesData[i].links[index][j][`type_label`] = `Drill into ${seriesData[i].data[index]}`;
+                        if (!seriesData[i].links[index][j][`type_label`]) seriesData[i].links[index][j][`type_label`] = `Drill into ${seriesData[i].name}`;
                         axis.links.push(seriesData[i].links[index][j]);
                     }
                 }
