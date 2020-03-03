1cfa61eb9497683387357baf64346608127b9c74
Kieran Schwegman <kschwegman@demandjump.com>
2020-03-03 09:48:04 -0500

    Adding data pull and configuration to single value metric for sparklines

--- deployable/graphs/apex/dj_arc_single_value_metric.js
+++ deployable/graphs/apex/dj_arc_single_value_metric.js
@@ -8,6 +8,57 @@ looker.plugins.visualizations.add({
             placeholder: 'Enter a name',
             hidden: false
         },
+
+        customSpacing: {
+            order: 10,
+            section: `Format`,
+            type: `sentence_maker`,
+            words: [
+                {type: 'separator', text: ' '}
+            ],
+            hidden: false
+        },
+
+        customLabel: {
+            order: 11,
+            section: `Format`,
+            type: `sentence_maker`,
+            words: [
+                {type: 'separator', text: 'Custom configuration:'}
+            ],
+            hidden: false
+        },
+
+        sparklines: {
+            order: 12,
+            section: `Format`,
+            type: `string`,
+            display: `select`,
+            values: [
+              {"None": "none"},
+              {"Bar chart": "normalBarChart"},
+              {"Bar chart with labels": "barWithLabels"},
+              {"Pie chart": "pieChart"},
+              {"Donut chart": "donutChart"},
+              {"Radial chart": "radialChart"},
+              {"Straight line": "straightLine"},
+              {"Straight line gradient": "straightLineGradient"}, 
+              {"Smooth line": "smoothLine"},
+              {"Smooth line gradient": "smoothLineGradient"},
+            ],
+            default: `none`,
+            hidden: false
+        },
+
+        doNotTruncate: {
+            label: `Don't Truncate data`,
+            order: 13,
+            section: `Format`,
+            type: `boolean`,
+            default: false,
+            hidden: false
+        },
+
     },
     create: function(element, config) {
         element.innerHTML = `
@@ -456,9 +507,527 @@ looker.plugins.visualizations.add({
         container[0].style.bottom = `${elementData.bottom}px`;
         container[0].style.right = `${elementData.right}px`;
 
-        console.log(`This is the new element data`, containerData);
+
+
+
+        // Find out whether it's a pivot
+        let pivot = false;
+        let pivotA = false;
+        let pivotB = false;
+        let doNotTruncateData = false;
+
+        if (config.doNotTruncate) doNotTruncate = config.doNotTruncate;
+
+        pivotCheck(); // Check for pivots
+        truncate(); // Truncate the data
+        
+
+        // Grab the series data
+        let seriesData = [];
+        let xaxis = [];
+
+        if (!pivot) {
+
+            // Series data structure
+            for(let i = 0; i < queryResponse.fields.measure_like.length; i++) {
+                let name = queryResponse.fields.measure_like[i].label;
+                if (queryResponse.fields.measure_like[i].label_short) name = queryResponse.fields.measure_like[i].label_short;
+
+                let obj = {name: name, className: name.replace(/ /g, `-`), data: [], links: []};
+                seriesData.push(obj);
+            }
+
+            // Series data
+            datum.forEach((row, index) => {
+                for(let i = 0; i < queryResponse.fields.measure_like.length; i++) {
+                    let value = 0;
+                    let lonks = row[queryResponse.fields.measure_like[i].name].links;
+                    if (lonks == null || lonks == undefined) lonks = [];
+                    if (row[queryResponse.fields.measure_like[i].name].value != null) value = row[queryResponse.fields.measure_like[i].name].value;
+                    seriesData[i].data.push(value);
+                    seriesData[i].links.push(lonks);
+                }
+            });
+
+            // Xaxis
+            datum.forEach(row => {
+                let nameVal = row[queryResponse.fields.dimension_like[0].name].value;
+                let lonks = row[queryResponse.fields.dimension_like[0].name].links;
+                if (lonks == null || lonks == undefined) lonks = [];
+
+                if (format == `dateime` && formatDates == true) {
+                    let sameMonthChecker = checkIfSameMonth();
+                    if (sameMonthChecker) xaxis.push({name: nameVal, links: lonks});
+                    else xaxis.push({name: convertDateTime(nameVal), links: lonks});
+                } else {
+                    xaxis.push({name: nameVal, links: lonks});
+                }
+            });
+
+
+        } else {
+            if (pivotA) {
+                // Labels
+                queryResponse.pivots.forEach(p => {
+                    let lonks = [];
+                    if (p.metadata[queryResponse.fields.pivots[0].name].links) lonks = p.metadata[queryResponse.fields.pivots[0].name].links;
+                    if (p.metadata[queryResponse.fields.pivots[0].name].rendered) {
+                        if (p.metadata[queryResponse.fields.pivots[0].name].rendered != null) xaxis.push({name: p.metadata[queryResponse.fields.pivots[0].name].rendered, links: lonks});
+                    } 
+                    else xaxis.push({name: p.key, links: lonks});
+                });
+    
+                // Series construct > the measure and the pivot for each key including data across all labels for each series(measure)
+                let series = [];
+                queryResponse.fields.measure_like.forEach((row, index) => {
+                    let obData = [];
+                    let liData = [];
+                    let newSeries = [];
+                    for(let i = 0; i < queryResponse.pivots.length; i++) {
+                        let keyname = queryResponse.pivots[i].key;
+                        let value = 0;
+
+                        liData.push(datum[0][row.name][keyname].links);
+                        
+                        if (datum[0][row.name][keyname].value != null) value = datum[0][row.name][keyname].value;
+                        obData.push(value);
+                    }
+
+                    let name = row.label;
+                    if (row.label_short) name = row.label_short;
+                    
+                    let obj = {
+                        name: name,
+                        className: name.replace(/ /g, `-`),
+                        data: obData,
+                        links: liData
+                    }
+                    seriesData.push(obj);
+                });
+            }
+            
+            if (pivotB) {
+                // Labels
+                datum.forEach(row => {
+                    let lonks = row[queryResponse.fields.dimension_like[0].name].links;
+                    if (row[queryResponse.fields.dimension_like[0].name].rendered) xaxis.push({name: row[queryResponse.fields.dimension_like[0].name].rendered, links: lonks});
+                    else xaxis.push({name: row[queryResponse.fields.dimension_like[0].name].value, links: lonks});
+                });
+
+                // Series Object
+                for(let i = 0; i < queryResponse.pivots.length; i++) {
+                    let name = queryResponse.pivots[i].data[queryResponse.fields.pivots[0].name];
+                    let obj = {
+                        name: name,
+                        className: name.replace(/ /g, `-`),
+                        data: [],
+                        links: []
+                    };
+                    seriesData.push(obj);
+                }
+
+                // Series data
+                datum.forEach(row => { 
+                    for(let i = 0; i < queryResponse.pivots.length; i++) seriesData[i].links.push(row[queryResponse.fields.measure_like[0].name][queryResponse.pivots[i].key].links);
+                });
+
+                datum.forEach(row => {
+                    for(let i = 0; i < queryResponse.pivots.length; i++) {
+                        let value = 0;
+                        if (row[queryResponse.fields.measure_like[0].name][queryResponse.pivots[i].key].value != null) value = row[queryResponse.fields.measure_like[0].name][queryResponse.pivots[i].key].value;
+                        seriesData[i].data.push(value);
+                    }
+                });
+            }
+        }
+
+        // Pull all the information into a single object
+        let seriesInformation = {
+            pivot: {
+                pivot: pivot,
+                pivotA: pivotA,
+                piovtB: pivotB
+            },
+            xaxis: xaxis,
+            data: seriesData
+        };
+        console.log(`This is the series information`);
+
+
+        /****************************************** 
+         * Functions
+        ******************************************/
+
+        function pivotCheck() {
+            if (queryResponse.fields.pivots.length != 0) {
+                pivot = true;
+                if (queryResponse.fields.dimension_like.length == 0) {
+                    pivotA = true;
+                    queryResponse.fields._dimension_like = queryResponse.fields.dimension_like;
+                    queryResponse.fields.dimension_like = queryResponse.fields.pivots;
+                } 
+                else pivotB = true;
+            }
+        }
+
+
+        function truncate() {
+            if (!doNotTruncate) {
+                for(let i = 0; i < queryResponse.fields.measure_like.length; i++) {
+                    let truncate = false;
+                    datum.forEach(row => {
+                        if (row[queryResponse.fields.measure_like[i].name].value > 100) truncate = true;
+                    });
+                    if (truncate) {
+                        datum.forEach(row => {
+                            row[queryResponse.fields.measure_like[i].name].original = row[queryResponse.fields.measure_like[i].name].value;
+                            row[queryResponse.fields.measure_like[i].name].value = Math.trunc(row[queryResponse.fields.measure_like[i].name].value); 
+                        });
+                    }
+                }
+            }
+        }
+
+
         
         /**************** Done! *****************/
         doneRendering(); 
     }
 });
+
+
+
+
+/* 
+
+        var gradientLineChart = {
+            chart: {
+                type: 'area',
+                height: 152,
+                sparkline: {
+                    enabled: true
+                },
+            },
+            colors: ["#3f6ad8"],
+            stroke: {
+                width: 2,
+                curve: 'smooth',
+            },
+            markers: {
+                size: 0
+            },
+            tooltip: {
+                fixed: {
+                    enabled: false
+                },
+                x: {
+                    show: false
+                },
+                y: {
+                    title: {
+                        formatter: function (seriesName) {
+                            return '';
+                        }
+                    }
+                },
+                marker: {
+                    show: false
+                }
+            },
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    shadeIntensity: 1,
+                    opacityFrom: 0.7,
+                    opacityTo: 0.9,
+                    stops: [0, 90, 100]
+                },
+            },
+            series: [{
+                data: sparklineData
+            }],
+            yaxis: {
+                min: 0
+            },
+        };
+
+
+
+
+        var dashSparkLines1 = { // Smooth line chart
+            chart: {
+                type: 'line',
+                height: 100,
+                sparkline: {
+                    enabled: true
+                },
+            },
+            colors: ["#3ac47d"],
+            stroke: {
+                width: 3,
+                curve: 'smooth',
+            },
+
+            markers: {
+                size: 0
+            },
+            tooltip: {
+                fixed: {
+                    enabled: true
+                },
+                x: {
+                    show: false
+                },
+                y: {
+                    title: {
+                        formatter: function (seriesName) {
+                            return '';
+                        }
+                    }
+                },
+                marker: {
+                    show: false
+                }
+            },
+            series: [{
+                data: sparklineData
+            }],
+            yaxis: {
+                min: 0
+            },
+        };
+
+
+
+
+
+        var straightLineChart = {
+          series: [{
+          data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54]
+        }],
+          chart: {
+          type: 'line',
+          width: 100,
+          height: 35,
+          sparkline: {
+            enabled: true
+          }
+        },
+        stroke: {
+            width: 2 
+        },
+        markers: {
+              size: 1.5, // Set size to zero for line chart with no markers
+              strokeColors: '#008EFA',
+        },
+        tooltip: {
+          fixed: {
+            enabled: false
+          },
+          x: {
+            show: false
+          },
+          y: {
+            title: {
+              formatter: function (seriesName) {
+                return ''
+              }
+            }
+          },
+          marker: {
+            show: false
+          }
+        }
+        };
+
+
+
+
+        var plainOlPieChart = {
+          series: [43, 32, 12, 9],
+          chart: {
+          type: 'pie',
+          width: 100,
+          height: 100,
+          sparkline: {
+            enabled: true
+          }
+        },
+        stroke: {
+          width: 1
+        },
+        tooltip: {
+          fixed: {
+            enabled: false
+          },
+        }
+        };
+
+
+
+         
+        var loadingRadial = {
+          series: [43],
+          chart: {
+          type: 'radialBar',
+          width: 100,
+          height: 10,
+          sparkline: {
+            enabled: true
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              margin: 0,
+              size: '50%'
+            },
+            track: {
+              margin: 0
+            },
+            dataLabels: {
+              show: false
+            }
+          }
+        }
+        };
+
+
+
+
+        var multiLoadingRadial = {
+          series: [53, 67, 22, 100],
+          chart: {
+          type: 'radialBar',
+          width: 150,
+          height: 150,
+          sparkline: {
+            enabled: true
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              margin: 0,
+              size: '50%'
+            },
+            track: {
+              margin: 1
+            },
+            dataLabels: {
+              show: false
+            }
+          }
+        }
+        };
+
+
+
+              
+        var normalRadial = {
+          series: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+          chart: {
+          type: 'donut',
+          width: 100,
+          height: 100,
+          sparkline: {
+            enabled: true
+          }
+        },
+        stroke: {
+          width: 1
+        },
+        tooltip: {
+          fixed: {
+            enabled: false
+          },
+        }
+        };
+
+
+
+
+        var fancyBarChart = {
+            chart: {
+                height: 265,
+                type: 'bar',
+                stacked: false,
+                toolbar: {
+                    show: false,
+                }
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                },
+
+            },
+            colors: ['#007bff', '#16aaff'],
+            stroke: {
+                width: 0,
+                colors: ['#fff'],
+                curve: 'smooth'
+            },
+            series: [{
+                name: 'Marine',
+                data: [44, 55, 41, 37, 22, 43]
+            }, {
+                name: 'Striking',
+                data: [53, 32, 33, 52, 13, 43]
+            },],
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return val + "K";
+                    }
+                }
+            },
+            fill: {
+                opacity: .8
+
+            },
+
+            legend: {
+                position: 'bottom',
+                horizontalAlign: 'center',
+            }
+        };
+
+
+
+
+        var normalBarChart = {
+            chart: {
+                type: 'bar',
+                height: 120,
+                sparkline: {
+                    enabled: true
+                },
+            },
+            tooltip: {
+                enabled: false,
+            },
+            stroke: {
+                width: 3,
+                curve: 'smooth'
+            },
+            colors: ['#007bff'],
+            markers: {
+                size: 0
+            },
+
+            series: [{
+                data: sparklineData
+            }],
+            yaxis: {
+                min: 0
+            },
+        };
+
+*/
